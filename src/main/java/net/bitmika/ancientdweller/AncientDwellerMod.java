2f579b61ce9188cf366d1392ec145fdbb884e140
mikavans <mikvanschalkwijk@gmail.com>
2024-05-09 20:29:23 +0200

    Followed course (no errors)

--- src/main/java/net/bitmika/ancientdweller/AncientDwellerMod.java
+++ src/main/java/net/bitmika/ancientdweller/AncientDwellerMod.java
@@ -1,6 +1,10 @@
 package net.bitmika.ancientdweller;
 
 import com.mojang.logging.LogUtils;
+import net.bitmika.ancientdweller.block.ModBlocks;
+import net.bitmika.ancientdweller.item.ModCreativeModeTabs;
+import net.bitmika.ancientdweller.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -26,6 +30,11 @@ public class AncientDwellerMod {
     public AncientDwellerMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -40,7 +49,20 @@ public class AncientDwellerMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
+        }
+        if(event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_ORE);
+            event.accept(ModBlocks.DEEPSLATE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.END_STONE_ALEXANDRITE_ORE);
+            event.accept(ModBlocks.NETHER_ALEXANDRITE_ORE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
